00b98713810ef33c14af811e42e32690b0dbef2f
MacXxs <mikipmoline@gmail.com>
2018-03-29 14:51:50 +0200

    Update DesastresHeuristicFunction.java

--- src/IA/Desastres/DesastresHeuristicFunction.java
+++ src/IA/Desastres/DesastresHeuristicFunction.java
@@ -5,6 +5,6 @@ import aima.search.framework.HeuristicFunction;
 public class DesastresHeuristicFunction implements HeuristicFunction{
 
     public double getHeuristicValue(Object n) {
-        return 0;
+        return ((DesastresBoard) n).getHeuristicValue();
     }
 }
