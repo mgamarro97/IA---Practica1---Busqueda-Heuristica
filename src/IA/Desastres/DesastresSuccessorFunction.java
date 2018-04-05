574cfa4105617174bc1364690ceeaa7e29d978ca
mgamarro97 <mariogamarro97@gmail.com>
2018-04-05 17:10:55 +0200

    Update DesastresSuccessorFunction.java

--- src/IA/Desastres/DesastresSuccessorFunction.java
+++ src/IA/Desastres/DesastresSuccessorFunction.java
@@ -14,19 +14,35 @@ public class DesastresSuccessorFunction implements SuccessorFunction {
 
         swapRescates(board, res, size);
         swapViajes(board, res);
-        setRescate(board, res, size);
         setViaje(board,res);
         return res;
     }
 
     private void swapRescates(DesastresBoard b, ArrayList res, int size){
         DesastresBoard aux;
-        for(int i = 0; i<size;i++){
-            for(int j = i+1; j<size;j++){
-                aux =  new DesastresBoard(b);
-                if(aux.swapR(i, j)) {
-                    String S = "Swapear rescates " + i + " y " + j;
-                    res.add(new Successor(S, aux));
+        int nHelicopteros = b.getNumHelicopteros();
+        for (int i = 0; i < nHelicopteros; i++){          //HELICOPTERO 1
+            int viajesHelicoptero1 = b.getNumViajes(i);  //VIAJE DE H1
+            for(int j = 0; j < viajesHelicoptero1; j++) {
+
+                for (int k = i+1; k < nHelicopteros; k++) {    //HELICOPTERO 2
+                    int viajesHelicoptero2 = b.getNumViajes(k);      //VIAJE DE H2
+                    for (int l = 0; l < viajesHelicoptero2; l++) {
+
+                        int grupo1[] = b.getGruposRescatados(i,j);
+                        for(int r = 0; r < 3;r++){
+                            int grupo2[] = b.getGruposRescatados(k,l);
+                            for (int s = r; s < 3;s++){
+                                if(grupo1[r] == -1 && grupo2[s] == -1)break;
+                                aux =  new DesastresBoard(b);
+                                if(aux.swapR(i, j, k, l, grupo1[r], grupo2[s])) { ///MODIFICAR MATRIZ DE VIAJES
+                                    String S = "Los helicopteros " + i + "y " + k + "cambian grupos " + r + "y " + s +
+                                            "de los viajes" + j + "y " + l;
+                                    res.add(new Successor(S, aux));
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -44,40 +60,8 @@ public class DesastresSuccessorFunction implements SuccessorFunction {
                     for (int l = 0; l < viajesHelicoptero2; l++) {
                         aux =  new DesastresBoard(b);
                         aux.swapV(i, j, k, l); //MODIFICAR MATRIZ DE VIAJES
-                        String S = "Swapear viajes " + i + " y " + j;
+                        String S = "Swapear viajes " + j + " y " + l + "de los helicopteros " + i + "y " + k;
                         res.add(new Successor(S, aux));
-                    }
-                }
-            }
-        }
-    }
-
-    private void setRescate(DesastresBoard b, ArrayList res, int size){
-        DesastresBoard aux;
-        int nHelicopteros = b.getNumHelicopteros();
-        for (int i = 0; i < nHelicopteros; i++){          //HELICOPTERO 1
-            int viajesHelicoptero1 = b.getNumViajes(i);  //VIAJE DE H1
-            for(int j = 0; j < viajesHelicoptero1; j++) {
-
-                for (int k = 0; k < nHelicopteros; k++) {    //HELICOPTERO 2
-                    int viajesHelicoptero2 = b.getNumViajes(k);      //VIAJE DE H2
-                    for (int l = 0; l < viajesHelicoptero2; l++) {
-                        aux =  new DesastresBoard(b);
-
-                        if(i != k && j != l){
-                            int grupo1[] = b.getGruposRescatados(i,j);
-                            for(int r = 0; r < 3;r++){
-                                if (grupo1[2] != -1)break;
-                                int grupo2[] = b.getGruposRescatados(k,l);
-                                if (grupo2[1] == -1)break;
-                                for (int s = r+1; s < 3;s++){
-                                    if(aux.setR(i, j, k, l, grupo1[r], grupo2[s])) { ///MODIFICAR MATRIZ DE VIAJES
-                                        String S = "Set viaje " + l + " a helicoptero " + i;
-                                        res.add(new Successor(S, aux));
-                                    }
-                                }
-                            }
-                        }
                     }
                 }
             }
